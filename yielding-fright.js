4f4219b9da68e8b719c5923ac85dfe4e9bb60216
Chelsey <jacobs27c@gmail.com>
2019-07-07 18:16:18 +0100

    Right click draws in white, left in black

--- yielding-fright.js
+++ yielding-fright.js
@@ -1,5 +1,11 @@
 const words = "We set sail on this new sea because there is new knowledge to be gained, and new rights to be won, and they must be won and used for the progress of all people. For space science, like nuclear science and all technology, has no conscience of its own. Whether it will become a force for good or ill depends on man, and only if the United States occupies a position of pre-eminence can we help decide whether this new ocean will be a sea of peace or a new terrifying theater of war. I do not say that we should or will go unprotected against the hostile misuse of space any more than we go unprotected against the hostile use of land or sea, but I do say that space can be explored and mastered without feeding the fires of war, without repeating the mistakes that man has made in extending his writ around this globe of ours.There is no strife, no prejudice, no national conflict in outer space as yet. Its hazards are hostile to us all. Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation may never come again. But why, some say, the Moon? Why choose this as our goal? And they may well ask, why climb the highest mountain? Why, thirty five years ago, fly the Atlantic? Why does Rice play Texas? We choose to go to the Moon! We choose to go to the Moon...We choose to go to the Moon in this decade and do the other things, not because they are easy, but because they are hard; because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one we intend to win, and the others, too.";
 
+window.oncontextmenu = function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+};
+
 const array = words.toUpperCase().replace(/[?.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").split(" ");
 let written = [];
 let wordSize = 12;
@@ -12,12 +18,14 @@ function draw() {
   background(220);
   for (let w of written) {
     textSize(w.size);
+    fill(w.colour);
     w.display();
   }
 }
 
 function mouseDragged() {
-  writeWord();
+  let clickType = (mouseButton === LEFT) ? true : false;
+  writeWord(clickType);
 }
 
 function mouseWheel(event) {
@@ -40,19 +48,27 @@ function findWord() {
   return array[index];
 }
 
-function writeWord() {
+function writeWord(clickType) {
   const w = new Word();
   let str = findWord();
-  written.push(w.setXYAndString(mouseX-20, mouseY, str, wordSize));
+  let colour = 0;
+  if (!clickType) {
+    colour = 255;
+  }
+  written.push(w.setXYAndString(mouseX-20, 
+                                mouseY, 
+                                str, 
+                                wordSize,
+                                colour));
   redraw();
 }
 
 class Word {
  // x and y values for position at which the word is drawn
-  constructor(x, y, str) { this.setXYAndString(x, y, wordSize); }
+  constructor(x, y, str, wordSize, colour) { this.setXYAndString(x, y, wordSize, colour); }
  
-  setXYAndString(x, y, str, wordSize) {
-    this.x = x, this.y = y, this.str = str, this.size = wordSize;
+  setXYAndString(x, y, str, wordSize, colour) {
+    this.x = x, this.y = y, this.str = str, this.size = wordSize, this.colour = colour;
     return this;
   }
  
